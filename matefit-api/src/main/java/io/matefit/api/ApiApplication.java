84a138509abb31f977f93585c2a4a49627855cf8
Byeongguk Gong <byeonggukgong@gmail.com>
2019-06-06 18:07:56 +0900

    프로젝트 생성

---
+++ matefit-api/src/main/java/io/matefit/api/ApiApplication.java
@@ -0,0 +1,11 @@
+package io.matefit.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
