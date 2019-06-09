d4f8d08845268e38d49455b98c89e8e675636b9a
Byeongguk Gong <byeonggukgong@gmail.com>
2019-06-09 16:45:22 +0900

    Core 모듈 컴포넌트 스캔 오류 수정

--- matefit-api/src/main/java/io/matefit/api/ApiApplication.java
+++ matefit-api/src/main/java/io/matefit/api/ApiApplication.java
@@ -2,7 +2,9 @@ package io.matefit.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("io.matefit")
 @SpringBootApplication
 public class ApiApplication {
     public static void main(String[] args) {
--- matefit-auth/src/main/java/io/matefit/auth/AuthApplication.java
+++ matefit-auth/src/main/java/io/matefit/auth/AuthApplication.java
@@ -2,7 +2,9 @@ package io.matefit.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("io.matefit")
 @SpringBootApplication
 public class AuthApplication {
     public static void main(String[] args) {
