6f36c75f36eee4f6a2906ac8c1d9f4b0995dd85a
Byeongguk Gong <byeonggukgong@gmail.com>
2019-07-20 16:58:59 +0900

    요청 형식 변경

--- matefit-api/src/main/java/io/matefit/api/controller/OAuthController.java
+++ matefit-api/src/main/java/io/matefit/api/controller/OAuthController.java
@@ -7,7 +7,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,7 +24,7 @@ public class OAuthController {
     /*
     카카오 회원가입 또는 로그인
      */
-    @GetMapping("/oauth/kakao")
+    @PostMapping("/oauth/kakao")
     public ResponseEntity<JwtToken> kakaoLogin(@RequestBody Map<String, Object> body) {
         return new ResponseEntity<JwtToken>(
                 jwtService.createToken(
